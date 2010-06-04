1811d25ec63a3490ae2bcecddbe36833e69a2772
Ryan Test <rfarley@customerfx.com>
2010-06-04 12:24:12 -0700

    Added OnCreate event code to set type

---
+++ Model/Entity Model/SalesLogix Application Entities/Account/Sage.SnippetLibrary.CSharp.@.fee03a9d-de91-47c2-aec6-893316d2d7ee.codesnippet.cs
@@ -0,0 +1,43 @@
+/*
+ * This metadata is used by the Sage platform.  Do not remove.
+<snippetHeader xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" id="fee03a9d-de91-47c2-aec6-893316d2d7ee">
+ <assembly>Sage.SnippetLibrary.CSharp</assembly>
+ <name>OnCreateStep</name>
+ <references>
+  <reference>
+   <assemblyName>Sage.Entity.Interfaces.dll</assemblyName>
+   <hintPath>%BASEBUILDPATH%\interfaces\bin\Sage.Entity.Interfaces.dll</hintPath>
+  </reference>
+  <reference>
+   <assemblyName>Sage.Form.Interfaces.dll</assemblyName>
+   <hintPath>%BASEBUILDPATH%\formInterfaces\bin\Sage.Form.Interfaces.dll</hintPath>
+  </reference>
+  <reference>
+   <assemblyName>Sage.Platform.dll</assemblyName>
+   <hintPath>%BASEBUILDPATH%\assemblies\Sage.Platform.dll</hintPath>
+  </reference>
+  <reference>
+   <assemblyName>Sage.SalesLogix.API.dll</assemblyName>
+  </reference>
+ </references>
+</snippetHeader>
+*/
+
+
+#region Usings
+using System;
+using Sage.Entity.Interfaces;
+using Sage.Form.Interfaces;
+using Sage.SalesLogix.API;
+#endregion Usings
+
+namespace Sage.BusinessRules.CodeSnippets
+{
+    public static partial class AccountBusinessRules
+    {
+        public static void OnCreateStep( IAccount account)
+        {
+            account.Type = "Hello";
+        }
+    }
+}
